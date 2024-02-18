ff1f7237b9af769f02e0eb3366e91916bcb08b04
savelgmx <savel@gmx.com>
2024-02-18 18:32:41 +0700

    added and improved data displaying fixed preview with mock data in Compose //TODO add data in Room db table

--- app/src/main/java/com/example/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.weatherforecast
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -21,12 +26,23 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.bumptech.glide.integration.compose.GlideImage
+import com.example.weatherforecast.response.Clouds
+import com.example.weatherforecast.response.Coord
+import com.example.weatherforecast.response.Main
+import com.example.weatherforecast.response.Sys
+import com.example.weatherforecast.response.Weather
 import com.example.weatherforecast.response.WeatherResponse
+import com.example.weatherforecast.response.Wind
 import com.example.weatherforecast.ui.theme.WeatherforecastTheme
 import com.example.weatherforecast.ui.viewmodel.OpenWeatherMapViewModel
+import com.example.weatherforecast.utils.AppConstants
 import com.example.weatherforecast.utils.Resource
 import com.example.weatherforecast.utils.WeatherUtils
 import dagger.hilt.android.AndroidEntryPoint
@@ -69,6 +85,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WeatherUI(weatherState: Resource<WeatherResponse>) {
+    val iconurl=AppConstants.WEATHER_API_IMAGE_ENDPOINT
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -79,12 +96,56 @@ fun WeatherUI(weatherState: Resource<WeatherResponse>) {
         when (weatherState) {
             is Resource.Success -> {
                 val temperature = weatherState.data?.main?.temp?.let { WeatherUtils.updateTemperature(it.toInt()) }
-                Text("Temperature: $temperature")
+                val name =weatherState.data?.name
+                val day= weatherState.data?.dt?.let { WeatherUtils.updateDateToToday(it.toInt()) }
                 val pressure = weatherState.data?.main?.pressure?.let { WeatherUtils.updatePressure(it.toInt()) }
-                Text( "Pressure: $pressure ")
+                val feels_like = weatherState.data?.main?.feels_like
 
-                //weatherState.data.weather[0].icon
-                //weatherState.data.weather[0].description
+                // Row 1: Name and Day with Blue Background
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                            .background(Color.Blue)
+                            .padding(horizontal = 10.dp, vertical = 5.dp)
+                    ) {
+                        Text(text = name!!, color = Color.White)
+                    }
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                            .background(Color.Blue)
+                            .padding(horizontal = 10.dp, vertical = 5.dp)
+                    ) {
+                        Text(text = day!!, color = Color.White)
+                    }
+                }
+
+                // Row 2: Temperature with Weather Icon
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "Temperature: $temperature",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        modifier = Modifier.padding(start = 8.dp)
+                    )
+                    /*
+                                         GlideImage(
+                                            model = "$iconurl${weatherState.data?.weather?.get(0)?.icon}.png",
+                                            contentDescription = "Weather Icon",
+                                            modifier = Modifier.size(50.dp)
+                                        )
+                    */
+                }
+
+                Text(text = "Feels_like: $feels_like", modifier = Modifier.padding(8.dp))
+                Text(text = " $pressure", modifier = Modifier.padding(8.dp))
 
             }
             is Resource.Loading -> {
@@ -103,9 +164,26 @@ fun WeatherUI(weatherState: Resource<WeatherResponse>) {
 
 @Preview(showBackground = true, device = "spec:width=411dp,height=891dp")
 @Composable
-fun WeatherUIPreview() {
-    val loadingState = Resource.Loading<WeatherResponse>()
-    WeatherUI(loadingState)
+fun WeatherUISuccessPreview() {
+    val successState = Resource.Success(getMockWeatherResponse())
+    WeatherUI(successState)
+}
+fun getMockWeatherResponse(): WeatherResponse {
+    return WeatherResponse(
+        Coord(92.7917, 56.0097),
+        listOf(Weather(804, "Clouds", "пасмурно", "04n")),
+        "stations",
+        Main(-21.77, -28.77, -22.78, -21.77, 1040, 85),
+        10000,
+        Wind(2.73, 228),
+        Clouds(100),
+        1708170884,
+        Sys(2, 2088371, "RU", 1708132314, 1708167250),
+        25200,
+        1502026,
+        "Красноярск",
+        200
+    )
 }
 
 
--- app/src/main/java/com/example/weatherforecast/response/WeatherResponse.kt
+++ app/src/main/java/com/example/weatherforecast/response/WeatherResponse.kt
@@ -50,7 +50,6 @@ data class Clouds(
 data class Sys(
     val type: Int,
     val id: Int,
-    val message: Double,
     val country: String,
     val sunrise: Long,
     val sunset: Long
