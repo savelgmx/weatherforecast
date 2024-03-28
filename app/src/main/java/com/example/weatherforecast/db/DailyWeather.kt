1d33710383662b706a7356ef5a3c5bf7ea8d7b30
a <savel@gmx.com>
2024-03-28 16:46:32 +0700

    Added Daily Weather entity to RoomDB //TODO rewrite Forecast Room Db with separate entities //and make written data more structurable

---
+++ app/src/main/java/com/example/weatherforecast/db/DailyWeather.kt
@@ -0,0 +1,60 @@
+package com.example.weatherforecast.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.example.weatherforecast.response.FeelsLike
+import com.example.weatherforecast.response.Temp
+
+@Entity(tableName = "daily_weather")
+@TypeConverters(Converters::class)
+data class DailyWeather(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name="clouds")
+    val clouds: Int,
+    @ColumnInfo(name="dew_point")
+    val dewPoint: Double,
+    // Include other fields as required
+    @ColumnInfo(name="dt")
+    val dt: Int,
+    @ColumnInfo(name="feels_like")
+    @Embedded
+    val feelsLike: FeelsLike= FeelsLike(0.0,0.0,0.0,0.0),
+    @ColumnInfo(name="humidity")
+    val humidity: Int,
+    @ColumnInfo(name="moon_phase")
+    val moonPhase: Double,
+    @ColumnInfo(name="moonrise")
+    val moonrise: Int,
+    @ColumnInfo(name="moonset")
+    val moonset: Int,
+    @ColumnInfo(name="pressure")
+    val pressure: Int,
+    @ColumnInfo(name="sunrise")
+    val sunrise: Int,
+    @ColumnInfo(name="sunset")
+    val sunset: Int,
+    @ColumnInfo(name="temp")
+    val temp: Temp,
+    @ColumnInfo(name="uvi")
+    val uvi: Double,
+    // Individual fields for the first weather object
+    @ColumnInfo(name = "weather_id")
+    val weatherId: Int,
+    @ColumnInfo(name = "weather_main")
+    val weatherMain: String,
+    @ColumnInfo(name = "weather_description")
+    val weatherDescription: String,
+    @ColumnInfo(name = "weather_icon")
+    val weatherIcon: String,
+
+    @ColumnInfo(name="wind_deg")
+    val windDeg: Int,
+    @ColumnInfo(name="wind_gust")
+    val windGust: Double,
+    @ColumnInfo(name="wind_speed")
+    val windSpeed: Double
+)
