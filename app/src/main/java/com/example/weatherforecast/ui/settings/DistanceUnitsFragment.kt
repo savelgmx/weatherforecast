0de8655ada37a53e90cd68027840699cb710f7af
a <savel@gmx.com>
2024-07-26 15:30:54 +0700

    Changed onCreateView return in DistanceUnitsFragment //TODO fix issue with navigation

--- app/src/main/java/com/example/weatherforecast/ui/settings/DistanceUnitsFragment.kt
+++ app/src/main/java/com/example/weatherforecast/ui/settings/DistanceUnitsFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.compose.ui.platform.ComposeView
+import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceFragmentCompat
 import com.example.weatherforecast.R
 import com.example.weatherforecast.components.DistanceUnitsScreen
@@ -18,29 +20,35 @@ class DistanceUnitsFragment : PreferenceFragmentCompat() {
     private val sharedViewModel: SharedViewModel
         get() = SharedViewModelHolder.sharedViewModel
 
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return ComposeView(requireContext()).apply {
-            setContent {
-                DistanceUnitsScreen(
-                    onDistanceUnitsChange = { newValue ->
-                        preferenceScreen.sharedPreferences.edit().putString("distance_units", newValue).apply()
-                        sharedViewModel.distanceUnitsLiveData.value = newValue
-                    },
-                    onWindSpeedChange = { newValue ->
-                        preferenceScreen.sharedPreferences.edit().putString("wind_speed", newValue).apply()
-                        sharedViewModel.windSpeedLiveData.value = newValue
-                    },
-                    onDone = {
-                        parentFragmentManager.popBackStack()
+        return super.onCreateView(inflater, container, savedInstanceState).apply {
+            val preferenceScreen = this@DistanceUnitsFragment.preferenceScreen
+
+            this?.findViewById<FrameLayout>(android.R.id.list_container)?.let {
+                it.addView(ComposeView(requireContext()).apply {
+                    setContent {
+                        DistanceUnitsScreen(
+                            onDistanceUnitsChange = { newValue ->
+                                preferenceScreen.sharedPreferences.edit().putString("distance_units", newValue).apply()
+                                sharedViewModel.distanceUnitsLiveData.value = newValue
+                            },
+                            onWindSpeedChange = { newValue ->
+                                preferenceScreen.sharedPreferences.edit().putString("wind_speed", newValue).apply()
+                                sharedViewModel.windSpeedLiveData.value = newValue
+                            },
+                            onDone = {
+                                findNavController().navigateUp()
+                            }
+                        )
                     }
-                )
-                }
+                })
             }
         }
-
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.preferences, rootKey)
     }
 }
